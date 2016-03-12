361b403e0fa966fe7208330d26974ad9eb0c62ac
ywKim <ywkim@ywkim-VirtualBox.(none)>
2016-03-12 10:41:16 +0900

    Add process id exception handling to the program
    Set process id to 0 when there is no sibling || parent || child process.

--- linux/kernel/prinfo.c
+++ linux/kernel/prinfo.c
@@ -6,6 +6,7 @@
  * 5. uaccess.h; definition of access_ok
  * add additional headers if judged to be necessary
  */
+
 #include <linux/prinfo.h>
 #include <linux/sched.h>
 #include <asm/errno.h>
@@ -17,16 +18,12 @@ int ptree(struct prinfo *buf, int *nr);
 static void DFS(struct task_struct *t, struct prinfo *buf, int size, int *nr);
 static struct prinfo task_to_info(struct task_struct *t);
 
-
-
 /* TODO:
  * error handling
  * setting pid to 0 when no parent || no children || no next sibling
  * make ptree return number of successes
  * system call number (384) assignment
  */
-
-
 
 int ptree(struct prinfo *buf, int *nr) {
   //checking the basic error conditions
@@ -39,18 +36,21 @@ int ptree(struct prinfo *buf, int *nr) {
 
   read_lock(&tasklist_lock);
 
-  DFS(&init_task, buf, size, nr);
+  DFS(&init_task, buf, size, nr, &init_task->sibling);
 
   read_unlock(&tasklist_lock);
 }
 
-static void DFS(struct task_struct *t, struct prinfo *buf, int size, int *nr) {
+/* sibling_head added to the argument of DFS
+ * in order to convey the information of first sibling when converting struct task_struct to struct prinfo
+ */
+static void DFS(struct task_struct *t, struct prinfo *buf, int size, int *nr, struct task_struct *sibling_head) {
   //if buffer is full return with the current buffer
   if (size == *nr) return;
 
   //add current task t to the buffer after changing it into struct prinfo
   //increase the number of tasks in the buffer afterwards.
-  struct prinfo info = task_to_info(t);
+  struct prinfo info = task_to_info(t, sibling_head);
   buf[*nr] = info;
   *nr += 1;
 
@@ -59,22 +59,23 @@ static void DFS(struct task_struct *t, struct prinfo *buf, int size, int *nr) {
   struct list_head *list;
   list_for_each (list, t->children) {
     child = list_entry (list, struct task_struct, sibling);
-    DFS(child, buf, size, nr);
+    DFS(child, buf, size, nr, t->children);
   }
 }
 
-static struct prinfo task_to_info(struct task_struct *t) {
+static struct prinfo task_to_info(struct task_struct *t, struct task_struct *sibling_head) {
   //change type task_struct into type prinfo 
   struct prinfo ret;
   struct task_struct *first_child = 
+    (t->children->next == t->children) ? NULL :
     list_entry (t->children->next, struct task_struct, sibling);
  
   ret.state = t->state;
   ret.pid = t->pid;
-  ret.parent_pid = t->parent->pid;
-  ret.first_child_pid = first_child->pid;
-  ret.next_sibling_pid = t->sibling->next->pid;
-  ret.uid = t->cred->uid;
+  ret.parent_pid = (t->real_parent == t) ? 0 : t->real_parent->pid;
+  ret.first_child_pid = (first_child == NULL) ? 0 : first_child->pid;
+  ret.next_sibling_pid = (t->sibling->next == sibling_head) ? 0 : t->sibling->next->pid;
+  ret.uid = t->real_cred->uid;
   ret.comm = t->comm;
 
   return ret;
