c08a353992458f4ecebf48f97a7ff5d7d94e6a4e
HURSUNGYUN <hursungyun@hursungyun-RF510-RF410-RF710.(none)>
2016-03-13 20:40:15 +0900

    Print all the processes

--- tests/test.c
+++ tests/test.c
@@ -5,16 +5,22 @@
 #include <linux/types.h>
 
 #define __NR_ptree 384
+#define __MAX__ 75
 
 int main()
 {
- int test_nr = 64;
- struct prinfo *test_buf = (struct prinfo*)malloc(sizeof(struct prinfo)*64);
+ int i, process;
+ int test_nr = __MAX__;
+ struct prinfo *test_buf = (struct prinfo*)malloc(sizeof(struct prinfo)*__MAX__);
  printf("Syscall started\n");
- printf("%d\n",syscall(__NR_ptree, test_buf, &test_nr));
- //printf("%d\n",syscall(__NR_ptree, NULL, NULL));
- printf("Syscall ended\n");
- printf("Root : %d Process ID : %d child ID: %d Process_Name : %s\n", test_buf->state, test_buf->pid, test_buf->first_child_pid, test_buf->comm);
+ process = syscall(__NR_ptree, test_buf, &test_nr);
+ printf("Syscall is done\nNow Print tasks\n");
+ printf("%s, %d, %d, %d, %d\n",test_buf[0].comm,test_buf[0].pid,test_buf[0].parent_pid,test_buf[0].first_child_pid,test_buf[0].next_sibling_pid);
+ for(i=1;i< ( (process < __MAX__) ? process : __MAX__ ) ;i++)
+ {
+   
+ printf("Name : %s, Pid: %d,Parent: %d,F.Child: %d,Next: %d\n",test_buf[i].comm,test_buf[i].pid,test_buf[i].parent_pid,test_buf[i].first_child_pid,test_buf[i].next_sibling_pid);
+ }
  
  return 0;
 }
