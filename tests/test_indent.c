612095bd7d36daf91ce31f6ff02d09e6a1fe149b
HURSUNGYUN <hursungyun@hursungyun-RF510-RF410-RF710.(none)>
2016-03-22 13:37:43 +0900

    Indentation test completed. minor error in test code modified.
     On branch test_indent
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   tests/test.c
    	new file:   tests/test_indent.c

--- tests/test_indent.c
+++ tests/test_indent.c
@@ -12,7 +12,31 @@ int main()
  int test_nr = __MAX__;
  struct prinfo *test_buf = (struct prinfo*)malloc(sizeof(struct prinfo)*__MAX__);
  printf("Syscall started\n");
- process = syscall(__NR_ptree, test_buf, &test_nr);
+ process = 14;
+
+   test_buf[0].pid = 0;
+   test_buf[0].parent_pid = 0;
+   test_buf[0].first_child_pid = 1;
+   test_buf[0].next_sibling_pid = process-1;
+
+ for(i=1;i<process-1;i++)
+ {
+   test_buf[i].pid = i;
+   test_buf[i].parent_pid = i-1;
+   test_buf[i].first_child_pid = i+1;
+   test_buf[i].next_sibling_pid = 0;
+ }
+
+   test_buf[process-1].pid = process-1;
+   test_buf[process-1].parent_pid = process-2;
+   test_buf[process-1].first_child_pid = 0;
+   test_buf[process-1].next_sibling_pid = 0;
+
+   test_buf[process].pid = process;
+   test_buf[process].parent_pid = 0;
+   test_buf[process].first_child_pid = 0;
+   test_buf[process].next_sibling_pid = 0;
+
  printf ("Total number of entries: %d\n", process);
  //checking the total number of entries before looping through the buffer.
  printf("Syscall is done\nNow Print tasks\n");
@@ -43,14 +67,14 @@ int main()
 		//print the process
     for(j=0;j<level;j++) printf("\t");
 //    printf("Name : %s, Pid: %d,Parent: %d,F.Child: %d,Next: %d\n",test_buf[i].comm,test_buf[i].pid,test_buf[i].parent_pid,test_buf[i].first_child_pid,test_buf[i].next_sibling_pid);
-    printf("%s,%d,%ld,%d,%d,%d,%d\n",test_buf[i].comm,test_buf[i].pid,test_buf[i].state,test_buf[i].parent_pid,test_buf[i].first_child_pid,test_buf[i].next_sibling_pid, test_buf[i].uid);
+    printf("%d,%d,%d,%d\n",test_buf[i].pid,test_buf[i].parent_pid,test_buf[i].first_child_pid,test_buf[i].next_sibling_pid);
 
    int t[2];
    t[0]=test_buf[i].parent_pid;
    t[1]=test_buf[i].next_sibling_pid;
 
 	 //if the process has no more siblings, pop the parent and decrease the number of tabs
-   while(stack[stackPointer-1][0]==t[0] && t[1]==0){
+   while((stack[stackPointer-1][0]==t[0] && t[1]==0)){
        t[0] = stack[stackPointer-1][1];
        t[1] = stack[stackPointer-1][3];
        level--;
