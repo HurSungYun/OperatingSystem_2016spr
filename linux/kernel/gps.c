9722d1a110eb8c905a88838d11a5faaf125b453a
ywKim <ywkim@ywkim-VirtualBox.(none)>
2016-05-30 14:49:50 +0900

    Implement set_gps_location and write the test code.
    * Creates a global variable that stores the current gps location in the kernel, and changes the variable value when the system call is called.
    * Writes the test code that round-robins through an array of gps_location to test the system call.

--- linux/kernel/gps.c
+++ linux/kernel/gps.c
@@ -4,8 +4,15 @@
 #include <linux/types.h>
 #include <linux/slab.h>
 #include <linux/gps.h>
+#include <linux/spinlock.h>
+
+struct gps_location curr;
+DEFINE_SPINLOCK(loc_lock);
 
 int set_gps_location(struct gps_location __user *loc)
 {
+	spin_lock(&loc_lock);
+	copy_from_user(&curr, loc, sizeof(struct gps_location));
+	spin_unlock(&loc_lock);
 	return 0;
 }
